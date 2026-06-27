38dc16930a160b7cb067c4b4601b4fb1c1068712
Nathan Graule <solarliner@gmail.com>
2026-06-27 14:41:40 +0200

    fix(build): move to async

--- make.py
+++ make.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 import os
 from shutil import which
 from pathlib import Path
@@ -5,57 +6,85 @@ import shlex
 from typing import Callable
 
 
-def exec(command: str, *args: str, check=True, shell=True) -> int:
-    import subprocess
+FILE = Path(__file__)
+ROOT_DIR = FILE.parent
+SRC_DIR = ROOT_DIR / "src"
+OUT_DIR = ROOT_DIR / "out"
 
-    cmd_str = shlex.join([command, *args])
-    print(cmd_str)
-    child_process = subprocess.Popen(cmd_str, shell=shell, encoding="utf-8")
-    retcode = child_process.wait()
+
+async def exec(command: str, *args: str, check=True, shell=True) -> int:
+    from asyncio import subprocess
+
+    cmd = shlex.join([command, *args])
+    proc = await subprocess.create_subprocess_shell(cmd)
+    retcode = await proc.wait()
     if check:
-        assert retcode == 0, "Child process did not exit successfully: " + cmd_str
+        assert retcode == 0, f"Process returned non-zero exit code: {cmd}"
     return retcode
 
 
-def compile(input: Path, format="pdf", typst: str | None = None) -> None:
+async def compile(input: Path, format="pdf", typst: str | None = None, command="compile") -> None:
     if not typst:
         typst = which("typst")
     assert typst is not None, f"typst required to generate {format} files"
 
-    src_dir = Path("src").absolute()
-    main_file = src_dir / "main.typ"
-    output = Path("out") / str(input.relative_to(src_dir)).replace(os.path.sep, " ")
-    output = output.with_suffix(f".{format}")
-    input = input.relative_to(src_dir)
+    def output_path(path: Path) -> Path:
+        out = OUT_DIR / str(path).replace(os.path.sep, " ")
+        return out.with_suffix(f".{format}")
+
+    input = input.relative_to(SRC_DIR)
+    output = output_path(input)
     print("Compiling", str(input), "->", str(output))
     output.parent.mkdir(parents=True, exist_ok=True)
-    _ = exec("typst", "compile",
+
+    if format == "html":
+        features_args = ["--features", "html"]
+    else:
+        features_args = []
+
+    match input.suffix:
+        case ".md":
+            main_file = SRC_DIR / "main.typ"
+            io_args = ["--input", f"file={input}", str(main_file), str(output)]
+        case ".typ":
+            io_args = [str(input), str(output_path(input))]
+        case suffix:
+            raise AssertionError(f"{suffix!r} input format not supported")
+
+    _ = await exec("typst", command,
+             *features_args,
              "--root", ".",
              "--font-path", "assets/fonts",
              "-f", format,
-             "--input", f"file={input}",
-             str(main_file),
-             str(output))
+             *io_args)
+
+
+def markdown_files() -> Iterable[Path]:
+    return SRC_DIR.glob("**/*.md")
+
+
+async def md2pdf(inputs: Iterable[Path], command = "compile"):
+    import asyncio
+
+    futures = [compile(input, format="pdf", command=command) for input in inputs]
+    _ = await asyncio.gather(*futures)
 
 
 if __name__ == "__main__":
-    from functools import partial, wraps
-    from multiprocessing import cpu_count
-    from threading import Semaphore, Thread
+    import asyncio
+    from argparse import ArgumentParser
+    import sys
 
-    sem = Semaphore(cpu_count())
-    def sync(f: Callable[[], None]):
-        @wraps(f)
-        def wrapper():
-            with sem:
-                f()
-        return wrapper
+    p = ArgumentParser()
+    p.add_argument("-w", "--watch", action="store_true", default=False)
 
-    def mkthread(f: Callable[[], None]):
-        t = Thread(target=f)
-        t.start()
-        return t
+    args = p.parse_args()
+    if args.watch:
+        command = "watch"
+    else:
+        command = "compile"
 
-    threads = [mkthread(sync(partial(compile, input))) for input in Path.cwd().joinpath("src").glob("**/*.md")]
-    for t in threads:
-        t.join()
+    try:
+        asyncio.run(md2pdf(markdown_files(), command=command))
+    except Exception as ex:
+        print(ex, file=sys.stderr)
